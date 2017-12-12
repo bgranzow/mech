d024dddfd9e06aeafc863b94e0589cd56bf603d8
Brian Granzow <brian.granzow@gmail.com>
2017-12-12 12:15:31 -0500

    qoi : allow setting of fields

--- qoi.hpp
+++ qoi.hpp
@@ -14,6 +14,7 @@ struct QoI<ST> : public Integrator {
   virtual ~QoI();
   ST const& get_qoi_value() const { return qoi_value; }
   ST const& get_elem_value() const { return elem_value; }
+  virtual void set_fields(Evaluators const&) {}
   virtual void set_time(double) {}
   virtual void set_elem_set(std::string const&) {}
   virtual void pre_process(LinAlg*);
@@ -35,6 +36,7 @@ struct QoI<FADT> : public Integrator {
   virtual ~QoI();
   ST const& get_qoi_value() const { return qoi_value; }
   FADT const& get_elem_value() const { return elem_value; }
+  virtual void set_fields(Evaluators const&) {}
   virtual void set_time(double) {}
   virtual void set_elem_set(std::string const&) {}
   virtual void pre_process(LinAlg*);
